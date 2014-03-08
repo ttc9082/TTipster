bb826c6cb94f2b2aaae9da086e1068414acf51b4
ttc9082 <tt1068@students.poly.edu>
2014-03-08 02:43:32 -0500

    ADD db module

---
+++ TTipster/db.h
@@ -0,0 +1,13 @@
+//
+//  db.h
+//  TTipster
+//
+//  Created by Tong Tianqi on 3/8/14.
+//  Copyright (c) 2014 tongtianqi. All rights reserved.
+//
+
+#import "FMDatabase.h"
+
+@interface db : FMDatabase
+
+@end
---
+++ TTipster/db.m
@@ -0,0 +1,13 @@
+//
+//  db.m
+//  TTipster
+//
+//  Created by Tong Tianqi on 3/8/14.
+//  Copyright (c) 2014 tongtianqi. All rights reserved.
+//
+
+#import "db.h"
+
+@implementation db
+
+@end
